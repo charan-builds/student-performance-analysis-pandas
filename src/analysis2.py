8bd8a31da67176aa7dc8e406b9920b7c97f92658
Derangula Charan Kumar <charan.derangula.tech@gmail.com>
2026-01-13 05:43:46 +0530

    Understand the Joins

--- src/analysis2.py
+++ src/analysis2.py
@@ -9,10 +9,19 @@ df_inner_join = pd.merge(df_marks,
                    on= "Student_Name",
                    how= "inner"
                 )
+#Print Inner Join
+print("\nInner Join")
+print(df_inner_join)
 df_left_join = pd.merge(df_marks,df_info,on="Student_Name",how = "left")
 print(df_left_join.isnull().sum())
 df_left_join["City"] = df_left_join["City"].fillna("Unknown")
 df_left_join["Class"] = df_left_join["Class"].fillna("Unknown")
 df_left_join["Total_Marks"] = df_left_join[["Maths","English","Science"]].sum(axis = 1)
 df_left_join["Percentage"] = (df_left_join["Total_Marks"] / 300) * 100
-print(df_left_join)
+print("Left Join")
+print(df_left_join[["Student_Name","Class","City","Maths","Science","English","Total_Marks","Percentage"]])
+df_left_join.to_csv("outputs/MasterFile.csv")
+outer_join = pd.merge(df_marks,df_info, on="Student_Name",how="outer")
+
+outer_join["City"] = outer_join["City"].fillna("Banglore")
+print(outer_join)
